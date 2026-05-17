f1b756540b971afc40bd46d06ad7e3c00264e9f7
Yureka <yuka@yuka.dev>
2026-05-17 20:08:50 +0200

    pty

--- src/main.rs
+++ src/main.rs
@@ -1,132 +1,278 @@
-use nom::HexDisplay;
-use nusb::transfer::{Bulk, ControlOut, ControlType, In, Out, Recipient};
-use nusb::{MaybeFuture, list_devices};
+mod pty;
+
+use std::pin::Pin;
+use anyhow::Context;
+use nusb::transfer::{Bulk, In, Out};
+use nusb::{list_devices};
 use std::collections::HashMap;
-use std::io::{Error, ErrorKind};
-use std::ops::Deref;
 use std::time::Duration;
-use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
-use zerocopy::FromBytes;
-use zerocopy_derive::{FromBytes, Immutable, KnownLayout};
+use tokio::io::{AsyncRead, AsyncWrite, AsyncWriteExt};
+use tokio_util::io::{SinkWriter, StreamReader, CopyToBytes};
+use zerocopy::{FromBytes, IntoBytes};
+use zerocopy_derive::{FromBytes, Immutable, KnownLayout, IntoBytes};
+use bytes::{Bytes, BytesMut, BufMut, Buf};
+use futures_util::{Sink, Stream, SinkExt, StreamExt, TryStreamExt};
 
 #[repr(C, packed)]
-#[derive(FromBytes, Default, Immutable, KnownLayout, Debug)]
+#[derive(FromBytes, Default, Immutable, KnownLayout, Debug, IntoBytes)]
 struct KisHeader {
-    sequence: u16,        // A sequence number
-    version: u8,          // Protocol version
-    portal: u8,           // The "portal" to connect to
-    argCount: u8,         // Number of arguments
-    indexLo: u8,          // An index
-    indexHiRplSizeLo: u8, // High 2 bits of index + low 6 bytes of reply size
-    rplSizeHi: u8,        // Reply size high bits: , number of words the device should send
-    reqSize: u32, // Size of the complete request: , including the arguments and payload: , excluding the header
-                  // Followed by arguments and payload data
+    sequence: u16,
+    version: u8,
+    portal: u8,
+    command: u8,
+    indexLo: u8,
+    indexHiRplSizeLo: u8,
+    rplSizeHi: u8,
+    reqSize: u32,
 }
 
-async fn debugusb_loop() -> anyhow::Result<()> {
-    let device = list_devices()
-        .await?
-        .find(|dev| dev.vendor_id() == 0x05ac && dev.product_id() == 0x1881)
-        .ok_or(anyhow::anyhow!("device not found"))?;
-    println!("open");
-    let device = device.open().await?;
-    println!("set_configuration");
-    /*
-        for config in device.configurations() {
-            println!("{:#?}", config);
-            println!("{}", config.configuration_value());
+async fn endpoint_tx(interface: &nusb::Interface, ep_id: u8) -> anyhow::Result<impl Sink<Bytes, Error = std::io::Error> + Send + 'static> {
+    let ep = interface.endpoint::<Bulk, Out>(ep_id)?;
+    let writer = ep.writer(16384);
+    Ok(futures_util::sink::unfold(writer, |mut writer, b: Bytes| async move {
+        writer.write_all(&b).await?;
+        writer.flush().await?;
+        Ok::<_, std::io::Error>(writer)
+    }))
+}
+
+async fn endpoint_rx(interface: &nusb::Interface, ep_id: u8) -> anyhow::Result<impl Stream<Item = std::io::Result<Bytes>> + Send + 'static> {
+    let ep = interface.endpoint::<Bulk, In>(ep_id | 0x80)?;
+    Ok(futures_util::stream::try_unfold(ep, |mut ep| async move {
+        while ep.pending() < 8 {
+            let buffer = ep.allocate(16384);
+            ep.submit(buffer);
         }
+        let buffer = ep.next_complete().await.into_result()?;
+        let data = Bytes::copy_from_slice(&buffer[..]);
+        ep.submit(buffer);
+        Ok(Some((data, ep)))
+    }))
+}
 
-        return Ok(());
-    */
-    device.set_configuration(1).await?;
-    println!("claim_interface");
+#[repr(u8)]
+enum KisPortal {
+    Config = 0x01,
+    //RSM = 0x10,
+    PAM = 0x11,
+    PPM = 0x13,
+}
 
-    let interface = device.claim_interface(0).await?;
+#[repr(u8)]
+enum KisCommand {
+    PCR = 0,
+    PCW = 1,
+    PAR = 2,
+    PAW = 3,
+    PSWD = 5,
+}
 
-    let mut rx_ep = interface.endpoint::<Bulk, In>(0x84)?;
-    tokio::task::spawn(async move {
-        let mut buf = None;
-        loop {
-            rx_ep.submit(buf.take().unwrap_or(rx_ep.allocate(262144)));
-            let retbuf = rx_ep.next_complete().await.into_result()?;
-            let (hdr, payload) = KisHeader::ref_from_prefix(&retbuf).unwrap();
+impl KisPortal {
+    fn endpoint_id(&self, device_version: u16) -> Option<u8> {
+        match (device_version, self) {
+            (_, KisPortal::Config) => Some(1),
 
-            if hdr.portal == 0x13 {
-                let words = u32::from_le_bytes(payload[..4].try_into().unwrap()) as usize;
-                let bytes = u32::from_le_bytes(payload[
-                    (words+1)*4..(words+2)*4
-                ].try_into().unwrap()) as usize;
-            //    println!("words: {}, bytes: {}", words, bytes);
-                std::io::Write::write_all(&mut std::io::stdout(), &payload[4..4+bytes])?;
+            // bcdDevice 1.20: M1 Pro
+            (0x120, KisPortal::PAM) => Some(1),
+            (0x120, KisPortal::PPM) => Some(2),
+
+            // bcdDevice 4.00: M4, A18 Pro
+            (0x400, KisPortal::PAM) => Some(3),
+            (0x400, KisPortal::PPM) => Some(4),
+            _ => None,
+        }
+    }
+}
+
+struct DebugUsb {
+    device_version: u16,
+    interface: nusb::Interface,
+    rx: HashMap<u8, Pin<Box<dyn Stream<Item = std::io::Result<Bytes>> + Send>>>,
+    tx: HashMap<u8, Pin<Box<dyn Sink<Bytes, Error = std::io::Error> + Send>>>,
+}
+
+impl DebugUsb {
+    async fn open() -> anyhow::Result<Self> {
+        let device = list_devices()
+            .await.context("list devices")?
+            .find(|dev| dev.vendor_id() == 0x05ac && dev.product_id() == 0x1881)
+            .ok_or(anyhow::anyhow!("device not found"))?;
+
+        let device = device.open().await.context("open device")?;
+        device.set_configuration(1).await.context("set configuration")?;
+        let interface = device.claim_interface(0).await.context("claim interface")?;
+
+        Ok(Self {
+            interface,
+            device_version: device.device_descriptor().device_version(),
+            rx: Default::default(),
+            tx: Default::default(),
+        })
+    }
+
+    async fn get_rx(&mut self, ep_id: u8) -> anyhow::Result<&mut Pin<Box<dyn Stream<Item = std::io::Result<Bytes>> + Send>>> {
+        match self.rx.entry(ep_id) {
+            std::collections::hash_map::Entry::Occupied(o) => Ok(o.into_mut()),
+            std::collections::hash_map::Entry::Vacant(v) => Ok(v.insert(Box::pin(endpoint_rx(&self.interface, ep_id).await?))),
+        }
+    }
+
+    async fn get_tx(&mut self, ep_id: u8) -> anyhow::Result<&mut Pin<Box<dyn Sink<Bytes, Error = std::io::Error> + Send>>> {
+        match self.tx.entry(ep_id) {
+            std::collections::hash_map::Entry::Occupied(o) => Ok(o.into_mut()),
+            std::collections::hash_map::Entry::Vacant(v) => Ok(v.insert(Box::pin(endpoint_tx(&self.interface, ep_id).await?))),
+        }
+    }
+
+    async fn tx(&mut self, ep_id: u8, msg: Bytes) -> anyhow::Result<()> {
+        self.get_tx(ep_id).await?.send(msg).await?;
+        Ok(())
+    }
+    
+    async fn rx(&mut self, ep_id: u8) -> anyhow::Result<Bytes> {
+        Ok(self.get_rx(ep_id).await?.next().await.unwrap()?)
+    }
+
+    async fn req(&mut self, ep_id: u8, msg: Bytes) -> anyhow::Result<Bytes> {
+        println!("out: {:x}", msg);
+        self.tx(ep_id, msg).await?;
+        tokio::time::timeout(Duration::from_millis(250),self.rx(ep_id)).await?
+    }
+
+    async fn enable_portals(&mut self, portals: impl IntoIterator<Item = KisPortal>) -> anyhow::Result<()> {
+        self.req(1, Bytes::from(&b"\x02\xff\xa0\x01\x01\x16\x04\x00\x04\x00\x00\x00\x00\x00\x03\x00"[..])).await?;
+        Ok(())
+    }
+
+    async fn uart_rx(&mut self) -> anyhow::Result<impl AsyncRead + Send + 'static> {
+        self.enable_portals([]).await?;
+        let pam_ep = KisPortal::PAM.endpoint_id(self.device_version).ok_or(anyhow::anyhow!("Do not know PAM portal endpoint for device version {:x}", self.device_version))?;
+        self.req(pam_ep, Bytes::from(&b"\x00\x13\xa0\x13\x01\x03\x04\x00\x04\x00\x00\x00\x01\x00\x00\x00"[..])).await?;
+
+        let ppm_ep = KisPortal::PPM.endpoint_id(self.device_version).ok_or(anyhow::anyhow!("Do not know PPM portal endpoint for device version {:x}", self.device_version))?;
+        let uart_rx = endpoint_rx(&self.interface, ppm_ep).await?;
+
+        let stream = uart_rx.try_filter_map(|mut retbuf| {
+            let hdr_bytes = retbuf.split_to(std::mem::size_of::<KisHeader>());
+            let hdr = KisHeader::ref_from_bytes(&hdr_bytes).unwrap();
+
+            if hdr.portal == KisPortal::PPM as u8 {
+                let words = retbuf.get_u32_le() as usize;
+                let mut content = retbuf.split_to(words*4);
+                let bytes = retbuf.get_u32_le() as usize;
+                content.truncate(bytes);
+
+                //print!("{}", String::from_utf8_lossy(&content));
+
+                futures_util::future::ready(Ok(Some(content)))
             } else {
-            println!("in 4:\n{:#x?}\n{}", hdr, payload.to_hex(16));
+                println!("in: {:#x?}", hdr);
+                futures_util::future::ready(Ok(None))
             }
-            buf = Some(retbuf);
-        }
-        Ok::<_, anyhow::Error>(())
-    });
+        });
 
-    let mut tx = HashMap::new();
-    let mut rx = HashMap::new();
-
-    let tx_ep = interface.endpoint::<Bulk, Out>(0x1)?;
-    let mut writer = tx_ep.writer(262144);
-    //let mut reader = rx_ep.reader(262144);
-    tx.insert(1, writer);
-
-    let mut rx_ep = interface.endpoint::<Bulk, In>(0x81)?;
-    let mut buf = None;
-    //let mut reader = rx_ep.reader(262144);
-    rx.insert(1, (rx_ep, buf));
-
-    let tx_ep = interface.endpoint::<Bulk, Out>(0x3)?;
-    let mut writer = tx_ep.writer(262144);
-    tx.insert(3, writer);
-
-    let mut rx_ep = interface.endpoint::<Bulk, In>(0x83)?;
-    let mut buf = None;
-    rx.insert(3, (rx_ep, buf));
-
-    for (ep, msg) in &[
-        (1,&b"\x02\xff\xa0\x01\x01\x16\x04\x00\x04\x00\x00\x00\x00\x00\x03\x00"[..]),
-        (3,&b"\x00\x13\xa0\x13\x01\x03\x04\x00\x04\x00\x00\x00\x01\x00\x00\x00"[..]),
-    ] {
-        println!("out {}:\n{}", ep, msg.to_hex(16));
-        let hdr = KisHeader::ref_from_prefix(msg).unwrap().0;
-        println!("{:#x?}", hdr);
-        tx.get_mut(ep).unwrap().write_all(msg).await?;
-        tx.get_mut(ep).unwrap().flush().await?;
-        //tokio::time::sleep(Duration::from_millis(150)).await;
-
-        let (rx_ep, buf) = rx.get_mut(ep).unwrap();
-
-        rx_ep.submit(buf.take().unwrap_or(rx_ep.allocate(262144)));
-        let Ok(retbuf) = tokio::time::timeout(Duration::from_millis(100), rx_ep.next_complete()).await else {
-            println!("ep {} timeout", ep);
-            continue;
-        };
-        let retbuf = retbuf.into_result()?;
-        println!("in {}:\n", ep); //, (&retbuf).to_hex(16));
-        let hdr = KisHeader::ref_from_prefix(&retbuf).unwrap().0;
-        println!("{:#x?}", hdr);
-        *buf = Some(retbuf);
+        Ok(StreamReader::new(stream))
     }
 
-    loop {
-        let (rx_ep, buf) = rx.get_mut(&1).unwrap();
-        rx_ep.submit(buf.take().unwrap_or(rx_ep.allocate(262144)));
-        let retbuf = rx_ep.next_complete().await.into_result()?;
-        println!("in 1:\n{}", (&retbuf).to_hex(16));
-        *buf = Some(retbuf);
+    async fn uart_tx(self) -> anyhow::Result<impl AsyncWrite + Send + 'static> {
+        let pam_ep = KisPortal::PAM.endpoint_id(self.device_version).ok_or(anyhow::anyhow!("Do not know PAM portal endpoint for device version {:x}", self.device_version))?;
+
+        let sink = futures_util::sink::unfold(self, move |mut dbgusb, mut b: Bytes| async move {
+
+            let with_padding = b.split_off(b.len() - b.len() % 4);
+            for msg in [b, with_padding] {
+                if msg.is_empty() { continue; }
+                for (hdr, second) in [
+                    (KisHeader {
+                        sequence: 0x1100,
+                        version: 0xa0,
+                        portal: KisPortal::PAM as u8,
+                        command: KisCommand::PAR as u8,
+                        indexLo: 0,
+                        indexHiRplSizeLo: 0,
+                        rplSizeHi: 0,
+                        reqSize: 0xc,
+                    }, &[0x2c, 0x40, 0x13, 0x48, 0x03, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00][..]),
+                    (KisHeader {
+                        sequence: 0x1155,
+                        version: 0xa0,
+                        portal: KisPortal::PAM as u8,
+                        command: KisCommand::PAR as u8,
+                        indexLo: 0,
+                        indexHiRplSizeLo: 0,
+                        rplSizeHi: 0,
+                        reqSize: 0xc,
+                    }, &[0x14, 0x40, 0x13, 0x48, 0x03, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00][..])
+                ] {
+                    let cmd = {
+                        let mut buf = BytesMut::new();
+                        buf.extend_from_slice(hdr.as_bytes());
+                        buf.extend_from_slice(second);
+
+                        buf.freeze()
+                    };
+                    let res = dbgusb.req(pam_ep, cmd).await.map_err(std::io::Error::other)?;
+                    println!("in:  {:x}", res);
+                }
+
+                let padding_bytes = (4 - (msg.len() % 4)) % 4;
+                let cmd = {
+                    let mut buf = BytesMut::new();
+
+                    buf.extend_from_slice(KisHeader {
+                        sequence: 0x1160,
+                        version: 0xa0,
+                        portal: KisPortal::PAM as u8,
+                        command: KisCommand::PAW as u8,
+                        indexLo: 0,
+                        indexHiRplSizeLo: 0,
+                        rplSizeHi: 0,
+                        reqSize: (12 + msg.len() + padding_bytes) as u32,
+                    }.as_bytes());
+
+                    // second header
+                    buf.put_u8((4*(4 - padding_bytes)) as u8);
+                    buf.extend_from_slice(&[0x40, 0x13, 0x48, 0x03, 0x00, 0x00, 0x00]);
+
+                    buf.put_u32_le((msg.len() + padding_bytes) as u32);
+    
+                    buf.put(msg);
+                    buf.put_bytes(0, padding_bytes);
+    
+                    buf.freeze()
+                };
+                let res = dbgusb.req(pam_ep, cmd).await.map_err(std::io::Error::other)?;
+                println!("in:  {:x}", res);
+            }
+            Ok::<_, std::io::Error>(dbgusb)
+        });
+
+        Ok(SinkWriter::new(CopyToBytes::new(sink)))
     }
+}
+
+
+async fn debugusb_loop(pty: &mut pty::Pty) -> anyhow::Result<()> {
+    let mut dbgusb = DebugUsb::open().await?;
+
+    let rx = dbgusb.uart_rx().await?;
+    let tx = dbgusb.uart_tx().await?;
+    let uart = tokio::io::join(rx, tx);
+    let mut uart = std::pin::pin!(uart);
+    tokio::io::copy_bidirectional(&mut uart, pty).await?;
     Ok(())
 }
 
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
+    let mut pty = pty::Pty::new()?;
+    println!("{}", pty.name());
+
+    tokio::fs::symlink(pty.name(), &"/dev/m1n1").await?;
+
     loop {
-        if let Err(e) = debugusb_loop().await {
+        if let Err(e) = debugusb_loop(&mut pty).await {
             println!("{:?}", e);
         }
         std::thread::sleep(Duration::from_secs(1));
---
+++ src/pty.rs
@@ -0,0 +1,116 @@
+use std::io;
+use std::io::{Read, Write};
+use std::task::ready;
+use std::pin::Pin;
+use std::task::Context;
+use std::task::Poll;
+use std::os::fd::OwnedFd;
+
+use nix::pty::{
+    posix_openpt,
+    grantpt,
+    unlockpt,
+    ptsname,
+    PtyMaster,
+};
+use nix::sys::stat::Mode;
+use nix::sys::termios::{
+    tcgetattr,
+    tcsetattr,
+    LocalFlags,
+};
+use nix::fcntl::{
+    open,
+    OFlag,
+};
+use nix::unistd::close;
+use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
+use tokio::io::unix::AsyncFd;
+
+pub struct Pty {
+    name: String,
+    inner: Option<AsyncFd<PtyMaster>>,
+    keep_alive: OwnedFd,
+}
+
+impl Pty {
+    pub fn new() -> io::Result<Pty> {
+        // Create a new PTY
+        let fd = posix_openpt(OFlag::O_RDWR | OFlag::O_NONBLOCK)?;
+
+        // Allow a client to connect
+        grantpt(&fd)?;
+        unlockpt(&fd)?;
+
+        // Get the pty path
+        let pty_name = unsafe { ptsname(&fd) }?;
+
+        // Keep one client connection open at all times, to prevent hang-up signals from triggering
+        // the tokio readiness
+        let client = open(pty_name.as_str(), OFlag::O_RDWR | OFlag::O_NOCTTY, Mode::empty())?;
+
+        Ok(Self {
+            name: pty_name,
+            inner: Some(AsyncFd::new(fd)?),
+            keep_alive: client,
+        })
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+impl AsyncRead for Pty {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>
+    ) -> Poll<io::Result<()>> {
+        loop {
+            let mut guard = ready!(self.inner.as_mut().unwrap().poll_read_ready(cx))?;
+
+            let unfilled = buf.initialize_unfilled();
+
+            match guard.try_io(|inner| inner.get_ref().read(unfilled)) {
+                Ok(Ok(len)) => {
+                    buf.advance(len);
+                    return Poll::Ready(Ok(()));
+                },
+                Ok(Err(err)) => return Poll::Ready(Err(err)),
+                Err(_would_block) => continue,
+            }
+        }
+    }
+}
+
+impl AsyncWrite for Pty {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8]
+    ) -> Poll<io::Result<usize>> {
+        loop {
+            let mut guard = ready!(self.inner.as_mut().unwrap().poll_write_ready(cx))?;
+
+            match guard.try_io(|inner| inner.get_ref().write(buf)) {
+                Ok(result) => return Poll::Ready(result),
+                Err(_would_block) => continue,
+            }
+        }
+    }
+
+    fn poll_flush(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<io::Result<()>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_shutdown(
+        self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+    ) -> Poll<io::Result<()>> {
+        Poll::Ready(Ok(()))
+    }
+}
